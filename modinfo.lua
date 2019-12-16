0511af3e436f4c1d2202bfe772826334a3e43d1f
jupitersh <arxhd.love@qq.com>
2019-12-16 21:26:11 +0800

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,19 @@
+name = "No Tree Regrowth"
+
+description = "Disable any kinds of tree respawning."
+
+author = "辣椒小皇纸"
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+version = "1.0"
+
+forumthread = ""
+
+api_version = 10
+
+dst_compatible = true
+
+all_clients_require_mod = false
+clients_only_mod = false
---
+++ modmain.lua
@@ -0,0 +1,1 @@
+GLOBAL.require("components/desolationspawner")._ctor = function() end
